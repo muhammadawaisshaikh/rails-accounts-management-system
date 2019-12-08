1524e5ae9d116ee1b13ecab4f30debf269b37d4c
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-08 12:29:18 +0500

    create method done with show

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -4,15 +4,25 @@ class JournalController < ApplicationController
   end
 
   def show
+    @journal = Journal.find(params[:id])
   end
 
   def new
+    @journal = Journal.new
   end
 
   def create
+    @journal = Journal.new(params_journal)
+
+    if @journal.save
+        redirect_to journal_index_path
+    else
+        render 'new'
+    end
   end
 
   def edit
+    @journal = Journal.find(params[:id])
   end
 
   def update
@@ -20,4 +30,11 @@ class JournalController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def params_journal
+      params.require(:journal).permit(:reference, :date, :summary, :amount)
+  end
+
 end

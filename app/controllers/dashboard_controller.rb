57925082ce40739caf747ca02493091c9e62f96e
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-09 18:43:15 +0500

    dashboard cards data getting in controller and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
   def index
+    @orders = Order.all.order(id: :asc)
+
+    @suppliers = Vendor.all.count
+
+    @all = Order.all.count;
+    @pending = Order.where(:status => 'pending').count;
+    @shipped = Order.where(:status => 'shipped').count;
+    @completed = Order.where(:status => 'completed').count;
+    @returned = Order.where(:status => 'returned').count;
+    @canceled = Order.where(:status => 'canceled').count;
   end
 end

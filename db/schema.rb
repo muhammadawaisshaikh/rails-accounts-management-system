b06e902dfad49e9ba8c2a3d772ebe49787b06c09
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-12 14:22:44 +0500

    deleted admins model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_104230) do
+ActiveRecord::Schema.define(version: 2019_12_12_092148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

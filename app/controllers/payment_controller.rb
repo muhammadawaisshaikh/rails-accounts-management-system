b7a3648f7b18a927454c1dc8339e21376e737fcf
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-12 15:12:37 +0500

    Payment controller and views created and CRUD in controller done

---
+++ app/controllers/payment_controller.rb
@@ -0,0 +1,52 @@
+class PaymentController < ApplicationController
+  def index
+    @payments = Payment.all.order(id: :asc)
+
+    @all = Payment.all.count;
+
+    search    
+  end
+
+  def show
+    @payment = Payment.find(params[:id])
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(params_payment)
+
+    if @payment.save
+        redirect_to payment_index_path
+    else
+        render 'new'
+    end
+  end
+
+  def edit
+    @payment = Payment.find(params[:id])
+  end
+
+  def update
+    @payment = Payment.find(params[:id])
+      if @payment.update(params_payment)
+        redirect_to payment_index_path
+      else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @payment = Payment.find(params[:id])
+    @payment.destroy
+    redirect_to payment_index_path
+  end
+
+  private
+
+  def params_payment
+    params.require(:payment).permit(:date, :type, :toFrom, :amount, :status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :order
   resources :dashboard
   resources :sales
+  resources :payment
   
 end

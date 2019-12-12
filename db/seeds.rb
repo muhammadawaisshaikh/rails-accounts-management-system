6b45c1abe26ae8435549ef02c2da9f390c43c157
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-12 15:04:07 +0500

    adding onInit Admin in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'looking up Super_admin ...'
+unless Admin.where(email: 'admin@shopkeryar.com').exists?
+  puts 'not found'
+  puts 'creating one...'
+  admin = Admin.new(email: 'admin@shopkeryar.com')
+  admin.password = 'Shopkeryar1234'
+  admin.save!
+end

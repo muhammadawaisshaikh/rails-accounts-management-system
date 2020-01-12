5a5d52696be1618752c030183f12a80611ebb0f1
muhammadawais <awaisshaikh94@gmail.com>
2020-01-12 22:35:21 +0500

    getting vendors in dropdown on edit and add order

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -2,4 +2,8 @@ module OrderHelper
     def products_for_select
         Product.all.collect { |p| [p.product_name, p.id] }
     end
+
+    def vendor_for_select
+        Vendor.all.collect { |v| [v.vendor_name, v.id] }
+    end
 end

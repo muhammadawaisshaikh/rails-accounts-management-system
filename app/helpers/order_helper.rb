73874bdba27faa40b5c183dc199790d0f52c632e
muhammadawais <awaisshaikh94@gmail.com>
2020-01-13 15:23:03 +0500

    helper modified

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,12 +1,6 @@
 module OrderHelper
     def products_for_select
-        Product.all.collect { |p|
-            if p.stock>0
-                [p.product_name, p.id]
-            else
-                # do nothing
-            end
-        }
+        Product.all.collect { |p| [p.product_name, p.id] }
     end
 
     def vendor_for_select

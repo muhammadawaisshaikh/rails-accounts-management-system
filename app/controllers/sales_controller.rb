d0c663303ce16b72a1c8d2c285b87c04edb9abda
muhammadawaisshaikh <awaisshaikh94@gmail.com>
2020-02-02 15:33:05 +0500

    sales controller fix

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,6 @@
 class SalesController < ApplicationController
   def index
-    @orders = Vendor.select("*").joins(:orders).group('orders.id', 'vendors.id').paginate(page: params[:page], per_page: 10);
+    @orders = Product.select("*").joins(:orders).group('orders.id', 'products.id').paginate(page: params[:page], per_page: 10);
 
     @sumTotal = Order.sum("amount");
     @completedTotal = Order.where("status = 'Completed'").sum("amount");
@@ -16,15 +16,15 @@ class SalesController < ApplicationController
   def search
     @search = params["search"]
     if @search.present?
-      @vendor_name = @search["vendor_name"]
-      @orders = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}%")
+      @product_name = @search["product_name"]
+      @orders = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}%")
 
-      @sumTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}%").sum("amount");
-      @completedTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}% AND status = 'Completed'").sum("amount");
-      @pendingTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}% AND status = 'Pending'").sum("amount");
-      @shippedTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}% AND status = 'Shipped'").sum("amount");
-      @returnedTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}% AND status = 'Returned'").sum("amount");
-      @canceledTotal = Vendor.select("*").joins(:orders).where("vendor_name ILIKE ?", "%#{@vendor_name}% AND status = 'Canceled'").sum("amount");
+      @sumTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}%").sum("amount");
+      @completedTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}% AND status = 'Completed'").sum("amount");
+      @pendingTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}% AND status = 'Pending'").sum("amount");
+      @shippedTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}% AND status = 'Shipped'").sum("amount");
+      @returnedTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}% AND status = 'Returned'").sum("amount");
+      @canceledTotal = Product.select("*").joins(:orders).where("product_name ILIKE ?", "%#{@product_name}% AND status = 'Canceled'").sum("amount");
     end
   end
 
